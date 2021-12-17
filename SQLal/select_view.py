e87c5543d6adfcc61b2580dec4b47398b2c1b620
juanfpena <62632100+juanfpena@users.noreply.github.com>
2021-12-17 12:09:26 -0300

    views corrections

--- SQLal/select_view.py
+++ SQLal/select_view.py
@@ -1,52 +1,28 @@
 """Select and show tables views"""
 from utils import engine
+import pandas as pd
 import sys
 
-
-def select_views(view_name):
-    """Select a table view"""
-    with engine.connect():
-        y = engine.execute("SELECT * FROM {}".format(view_name)).fetchall()
-        for item in y:
-            print(item)
-    print(view_name, "\n")
-
-
 list_of_views = [
-    "income_by_month",
-    "expenses_by_month",
-    "income_statement_by_month",
-    "income_by_year",
-    "expenses_by_year",
-    "income_statement_by_year",
-    "revenue_growth"
+    ['1', "income_by_month"],
+    ['2', "expenses_by_month"],
+    ['3', "income_statement_by_month"],
+    ['4', "income_by_year"],
+    ['5', "expenses_by_year"],
+    ['6', "income_statement_by_year"],
+    ['7', "revenue_growth"]
 ]
 
 
 if __name__ == "__main__":
 
     """Call a table view"""
-    if len(sys.argv) == 3:
-        tag1 = sys.argv[1]
-        tag2 = sys.argv[2]
+    for id, table in list_of_views:
 
-        if tag1 == "year":
-            if tag2 == "1":
-                select_views("income_by_year")
-            if tag2 == "2":
-                select_views("expenses_by_year")
-            if tag2 == "3":
-                select_views("income_statement_by_year")
+        if id == sys.argv[1]:
+            df = pd.read_sql(f'SELECT * FROM {table};', con=engine)
 
-        if tag1 == "month":
-            if tag2 == "1":
-                select_views("income_by_month")
-            if tag2 == "2":
-                select_views("expenses_by_month")
-            if tag2 == "3":
-                select_views("income_statement_by_month")
-    else:
-        tag1 = sys.argv[1]
-        if tag1 == "all":
-            for vw_name in list_of_views:
-                select_views(vw_name)
+            print(df)
+
+    if sys.argv not in [i[0] for i in list_of_views]:
+        print('Id not recognized.')
