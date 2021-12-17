5275b22a80bcfac225cb2531f65fa157646a2c6a
juanfpena <62632100+juanfpena@users.noreply.github.com>
2021-12-17 09:11:18 -0300

    views corrected

--- SQLal/SQL_views/income_statement_by_year.sql
+++ SQLal/SQL_views/income_statement_by_year.sql
@@ -1,10 +1,19 @@
 CREATE OR REPLACE VIEW income_by_year AS
-SELECT year(created_at) AS 'year_income', 
-    SUM(price) AS 'sales', 
-    SUM(cost) AS 'cost_of_goods_sold', 
-    (SUM(price) - SUM(cost)) AS 'gross_profit'
-    FROM sale
-    JOIN product ON sale.product_id = product.id
+SELECT year(s.created_at) AS 'year_income', 
+    SUM(product.price * p_cost.quantity) AS 'sales',
+    SUM(p_cost.total_cost) AS 'cost_of_goods_sold',
+    (SUM(product.price * p_cost.quantity) - SUM(p_cost.total_cost)
+    ) AS 'gross_profit'
+    FROM sale AS s
+    JOIN (
+    SELECT sp.sale_id, sp.quantity, (SUM(p.cost * p.quantity)) AS 'total_cost' FROM sale_to_purchase AS sp
+    JOIN purchase AS p
+    ON sp.purchase_id = p.id
+    GROUP BY sale_id
+    ) AS p_cost
+    ON s.id = p_cost.sale_id
+    JOIN product
+    ON s.product_id = product.id
     GROUP BY 1
     ORDER BY 1;
 #
@@ -39,8 +48,8 @@ SELECT year_income AS 'year',
     other_expenses,
     total_expenses,
     (gross_profit - total_expenses) AS 'profits'
-    FROM income_by_year AS i
-    JOIN expenses_by_year AS e 
-    ON i.year_income = e.year_expense AND i.year_income = e.year_expense
+    FROM income_by_month AS i
+    JOIN expenses_by_month AS e 
+    ON i.year_income = e.year_expense
     GROUP BY 1
     ORDER BY 1;
--- SQLal/create_tables.py
+++ SQLal/create_tables.py
@@ -3,4 +3,6 @@ from utils import engine, Base
 import models
 
 """Create all tables"""
-Base.metadata.create_all(engine)
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
